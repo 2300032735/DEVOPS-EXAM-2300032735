39f2c31f44d1da1fc194fa2ed237b54fbf31ca6c
= <=>
2025-10-24 17:28:28 +0530

    modified App.jsx

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,16 +1,3 @@
-import VoterApp from "./VoterApp"
-
-
-function App() {
-  
-
-  return (
-    <>
-      <div>
-        <VoterApp/>
-        </div>
-    </>
-  )
-}
-
-export default App
+// src/config.js
+const BASE_URL = "http://localhost:2030/2300032735-backend/"; // or your backend URL
+export default BASE_URL;
